ce05bbae094a5cf4c5f51e3915c3179eb2fb3816
Sachin-0099 <itssachin0099@gmail.com>
2023-11-25 22:59:05 +0530

    inital commit

---
+++ add.java
@@ -0,0 +1,12 @@
+package JavaBasics;
+
+public class add {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a =5;
+int b=9;
+int sum=a+b;
+System.out.println(sum);
+
+	}
+}
---
+++ average.java
@@ -0,0 +1,17 @@
+package JavaBasics;
+    import java.util.Scanner;
+public class average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc =new Scanner(System.in);
+System.out.println("Enter first number");
+int m1=sc.nextInt();
+System.out.println("Enter Second number");
+int m2=sc.nextInt();
+System.out.println("Enter Third number");
+int m3=sc.nextInt();
+System.out.println("Average of three no.is:"+(m1+m2+m3)/3);
+
+	}
+}
