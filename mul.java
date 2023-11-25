ab2c3f8761d3fb61b018c1820e055eb331bcf8e4
Sachin-0099 <itssachin0099@gmail.com>
2023-11-25 23:13:11 +0530

    commit

---
+++ mul.java
@@ -0,0 +1,14 @@
+package JavaBasics;
+import java.util.*;
+public class mul {
+    public static void main(String args []){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number");
+        int a=sc.nextInt();
+        System.out.println("Enter second number");
+        int b=sc.nextInt();
+        int mul=a*b;
+        System.out.println("Multiply of two number is:"+mul);
+            }
+        }
+  
