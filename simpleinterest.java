de1967f932f43eb90e3c71d5c235502584d58f44
Sachin-0099 <itssachin0099@gmail.com>
2023-11-26 00:01:50 +0530

    commit si

---
+++ simpleinterest.java
@@ -0,0 +1,14 @@
+package JavaBasics;
+
+public class simpleinterest {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double p=5000;
+double r=2;
+double t=4;
+double si=(p*r*t)/100;
+System.out.println(si);
+	}
+
+}
+
