7a90450b4a630a20947bbf631264f0546db04f9f
Sachin-0099 <itssachin0099@gmail.com>
2023-11-27 00:24:30 +0530

    commit

---
+++ gameofpattern.java
@@ -0,0 +1,15 @@
+package JavaBasics;
+import java.util.*;
+public class gameofpattern {
+    public static void main(String args []){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the number");
+int n=sc.nextInt();
+for(int i=0;i<=3;i++){
+    for(int j=0;j<=3;j++){
+        System.out.print("#");
+    }
+    System.out.println();
+}
+    }
+}
