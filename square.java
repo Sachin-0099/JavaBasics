a2df57631ab0814bd25d50bc0b620b0bfbabe9ef
Sachin-0099 <itssachin0099@gmail.com>
2023-11-27 00:30:50 +0530

    Inital commit

---
+++ square.java
@@ -0,0 +1,20 @@
+package JavaBasics;
+import java.util.*;
+public class square {
+    public static void main(String args []){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter n");
+    int n=sc.nextInt();
+    int i=1;
+    while(i<=n){
+int j=1;
+while(j<=n){
+    System.out.print(n);
+    j++;
+}
+System.out.println();
+i++;
+    }
+
+}
+}
