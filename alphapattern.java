22d1d86ef665309febd4389be58334c9e57b0ed5
Sachin-0099 <itssachin0099@gmail.com>
2023-11-28 16:01:00 +0530

    Initial commit

---
+++ alphapattern.java
@@ -0,0 +1,13 @@
+package JavaBasics;
+
+public class alphapattern {
+    public static void main(String args []){
+        int n=3;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print((char)('A'+i-1));
+            }
+            System.out.println();
+        }
+    }
+}
