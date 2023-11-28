fceeb5d64c1d735255695b6d270e0e14ba65374a
Sachin-0099 <itssachin0099@gmail.com>
2023-11-28 16:38:14 +0530

    Initial commit

---
+++ invertednumpattern.java
@@ -0,0 +1,19 @@
+package JavaBasics;
+
+public class invertednumpattern {
+    public static void main(String args []){
+        int n=5;
+        int i=n;
+        while(i>0){
+            int j=1;
+            while(j<=i){
+         System.out.print(i);
+         j++;
+} 
+System.out.println();
+
+i--;    
+   }
+
+    }
+}
